a22063fbea056c892169ec4033b88a81d87d079a
farrel irawan <irawanssfarrel@gmail.com>
2025-04-06 00:54:53 +0700

    menyelesaikan JAVA_GUI

--- src/frame/MyFrame.java
+++ src/frame/MyFrame.java
@@ -4,10 +4,16 @@ import java.awt.Color;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class MyFrame extends JFrame {
 
     public MyFrame() {
+
+        
+        JLabel MyLabel = new JLabel();
+        MyLabel.setText("Hello Everyone, this is my first Java GUI");
+        MyLabel.setBounds(50, 50, 500, 50);
 
         this.setTitle("Farrel Java GUI");  //set the title on frame
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //set the default operation while close
@@ -20,6 +26,8 @@ public class MyFrame extends JFrame {
 
         ImageIcon icon = new ImageIcon("src/assets/farrel.jpg");
         this.setIconImage(icon.getImage());
+
+        this.add(MyLabel);
 
     }
 
