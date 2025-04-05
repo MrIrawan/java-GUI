472e135bb66d9a8ef106619211244f2c68b55488
farrel irawan <irawanssfarrel@gmail.com>
2025-04-06 00:36:38 +0700

    menambahkan assets logo dan membuat package label untuk menyimpan class MyLabel

--- src/frame/MyFrame.java
+++ src/frame/MyFrame.java
@@ -1,16 +1,25 @@
 package frame;
 
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class MyFrame extends JFrame {
 
     public MyFrame() {
 
-        this.setTitle("Farrel Java GUI");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setResizable(false);
-        this.setSize(700, 450);
-        this.setVisible(true);
+        this.setTitle("Farrel Java GUI");  //set the title on frame
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //set the default operation while close
+        this.setResizable(false);  //set the resize option for frame
+        this.setSize(700, 450);  //set the size of frame
+        this.setVisible(true);  //set the visibility of frame
+
+
+        this.getContentPane().setBackground(new Color(0xe0e7ff));  //set the background color for the frame
+
+        ImageIcon icon = new ImageIcon("src/assets/farrel.jpg");
+        this.setIconImage(icon.getImage());
 
     }
 
---
+++ src/label/MyLabel.java
@@ -0,0 +1,13 @@
+package label;
+
+import javax.swing.JLabel;
+
+public class MyLabel extends JLabel {
+
+    public MyLabel() {
+
+        this.setText("Hello Guys, My name is Farrel");
+
+    }
+
+}
