eb4aad3bbe77779047677cae570aff7dd897d985
farrel irawan <irawanssfarrel@gmail.com>
2025-04-05 12:07:57 +0700

    membuat style default untuk frame

--- src/frame/MyFrame.java
+++ src/frame/MyFrame.java
@@ -3,5 +3,15 @@ package frame;
 import javax.swing.JFrame;
 
 public class MyFrame extends JFrame {
-    
+
+    MyFrame() {
+
+        this.setTitle("Farrel Java GUI");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setSize(600, 400);
+        this.setVisible(true);
+
+    }
+
 }
